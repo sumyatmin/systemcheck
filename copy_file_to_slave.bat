98e107b69211889a3a05dea7b9edc49004eea0be
sumyatmin <sumyatmin@gmail.com>
2021-03-28 22:52:09 +1100

    Update copy_file_to_slave.bat

--- copy_file_to_slave.bat
+++ copy_file_to_slave.bat
@@ -1,1 +1,1 @@
-pscp -scp C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\Sample_MasterWin_SlaveCentos\get_mem_stat.sh soepapa@192.168.251.210:/home/soepapa/Jenkins/workspace/
+pscp -scp C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\Sample_MasterWin_SlaveCentos\get_mem_stat.sh soepapa@192.168.251.210:/home/soepapa/jenkins/workspace/
